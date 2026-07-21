9770947421174988b5f29cf98d0412ddd78b9bae
Ryan TIPVEAU <ryan12052002@gmail.com>
2026-07-21 18:02:40 +0200

    Update User model for UTC timestamps and comments

--- backoffice/models/user.py
+++ backoffice/models/user.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 from database.db import db
 
@@ -7,12 +7,10 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-
     id = db.Column(
         db.Integer,
         primary_key=True
     )
-
 
     username = db.Column(
         db.String(80),
@@ -20,19 +18,16 @@ class User(db.Model):
         nullable=False
     )
 
-
     email = db.Column(
         db.String(120),
         unique=True,
         nullable=False
     )
 
-
     password_hash = db.Column(
         db.String(255),
         nullable=False
     )
-
 
     role = db.Column(
         db.String(20),
@@ -40,29 +35,25 @@ class User(db.Model):
         default="employee"
     )
 
-
     branch_id = db.Column(
         db.Integer,
         db.ForeignKey("branches.id"),
         nullable=True
     )
 
-
-    # Soft delete
+    # Suppression logique
     is_deleted = db.Column(
         db.Boolean,
         default=False
     )
 
-
     created_at = db.Column(
         db.DateTime,
-        default=datetime.utcnow
+        default=lambda: datetime.now(UTC)
     )
-
 
     updated_at = db.Column(
         db.DateTime,
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC)
     )
