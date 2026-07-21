ac9f6ab226f525072eb10da9ee23efc5bd7c9670
Melopheelo12 <ryan12052002@gmail.com>
2026-07-21 17:55:10 +0200

    create new file to represent db in pyhton class

---
+++ backoffice/models/user.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+
+from database.db import db
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+
+    username = db.Column(
+        db.String(80),
+        unique=True,
+        nullable=False
+    )
+
+
+    email = db.Column(
+        db.String(120),
+        unique=True,
+        nullable=False
+    )
+
+
+    password_hash = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+
+    role = db.Column(
+        db.String(20),
+        nullable=False,
+        default="employee"
+    )
+
+
+    branch_id = db.Column(
+        db.Integer,
+        db.ForeignKey("branches.id"),
+        nullable=True
+    )
+
+
+    # Soft delete
+    is_deleted = db.Column(
+        db.Boolean,
+        default=False
+    )
+
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
