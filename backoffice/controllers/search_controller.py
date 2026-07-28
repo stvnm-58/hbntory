07cee44dadd9bdfaa475351830af1065889ab813
Anthodido <anthony.didomenico678@gmail.com>
2026-07-28 15:42:23 +0200

    correction

--- backoffice/controllers/search_controller.py
+++ backoffice/controllers/search_controller.py
@@ -6,9 +6,6 @@ from services.search_service import search_products
 def search():
     query = request.args.get("q", "").strip()
 
-    if not query:
-        return jsonify({"error": "Le paramètre 'q' est obligatoire."}), 400
-
     try:
         results = search_products(query)
         return jsonify(results), 200
