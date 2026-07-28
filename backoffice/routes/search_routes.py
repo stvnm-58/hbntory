7e7ddad928abdbdad0197339b643a3be049aafcd
Melopheelo12 <ryan12052002@gmail.com>
2026-07-28 14:36:35 +0200

    add search

---
+++ backoffice/routes/search_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+from controllers.search_controller import search
+
+
+search_bp = Blueprint(
+    "search",
+    __name__
+)
+
+
+search_bp.route(
+    "/",
+    methods=["GET"]
+)(search)
