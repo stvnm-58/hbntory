9e77931436a4682c5c1b24bcd2dd3ad6589417a3
Anthodido <anthony.didomenico678@gmail.com>
2026-07-28 14:42:00 +0200

    add 2 line

--- dev.sh
+++ dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 trap 'kill 0' EXIT
 
-(cd client_web && python3 -m http.server 8000) &
+(cd api_extern && docker compose up --build) &
 (cd backoffice && python3 app.py) &
+(cd product_mcp_server && source venv/bin/activate && uvicorn app:app --host 0.0.0.0 --port 8000 --reload) &
+(cd client_web && python3 -m http.server 8080) &
 
 wait
