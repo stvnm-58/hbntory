226c8565a42edd506b158cd3bba9bad20a9d59c1
Melopheelo12 <ryan12052002@gmail.com>
2026-07-28 23:04:02 +0200

    raccourci 1ligne

--- dev.sh
+++ dev.sh
@@ -2,7 +2,7 @@
 trap 'kill 0' EXIT
 
 (cd api_extern && docker compose up --build) &
-(cd backoffice && python3 app.py) &
+(cd backoffice && source venv/bin/activate && python3 app.py) &
 (cd product_mcp_server && source venv/bin/activate && uvicorn app:app --host 0.0.0.0 --port 8000 --reload) &
 (cd client_web && python3 -m http.server 8080) &
 
